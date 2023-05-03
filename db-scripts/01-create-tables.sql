5e8d4becd096c26dd4cabc4074ccda8b95b8e6aa
Hajr Delli <hajr.delli@accenture.com>
2023-05-03 16:50:22 +0100

    added venue request table

--- db-scripts/01-create-tables.sql
+++ db-scripts/01-create-tables.sql
@@ -10,3 +10,14 @@ CREATE TABLE venues (
     end_date INTEGER NOT NULL,
     PRIMARY KEY(venue_id)
 );
+
+CREATE TABLE venue_requests (
+    request_id INTEGER GENERATED ALWAYS AS IDENTITY,
+    artist_id INTEGER NOT NULL,
+    venue_id INTEGER NOT NULL,
+    artist_genre VARCHAR(255) NOT NULL,
+    approval_status BOOLEAN NOT NULL,
+    PRIMARY KEY(request_id),
+    CONSTRAINT fk_artist FOREIGN KEY(artist_id) REFERENCES artists(artist_id),
+    CONSTRAINT fk_venue FOREIGN KEY(venue_id) REFERENCES venues(venue_id)
+);
