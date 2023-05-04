27231304da4e3ccc0d8bfeb06ed2ffafa0123da5
Anees Hussain <anees.hussain@accenture.com>
2023-05-04 15:15:08 +0100

    git playing up, only 1 commit

--- Frontend/src/components/VenueItem.tsx
+++ Frontend/src/components/VenueItem.tsx
@@ -4,10 +4,14 @@ export type VenueItemProps = {
 }
 
 const VenueItem = (props: VenueItemProps) => {
+    const baseUrl = 'localhost:3000'
+    const venueRename = props.venueName.replaceAll(' ', '').toLowerCase()
+    const fullUrl = baseUrl + '/' + venueRename
+
     return (
         <li>
           <h4>{props.venueName}</h4>
-          <button className='btn btn-primary' type='button' >Open Venue Details</button>
+          <button className='btn btn-primary' type='button' onClick={() => window.open(fullUrl)}>View Venue</button>
         </li>
     )
 }
