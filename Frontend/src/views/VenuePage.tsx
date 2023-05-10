c33866263bb4682ccc33c6fa73ac4193c6183d02
Ali <mustafa.b.ali@accenture.com>
2023-05-10 13:46:13 +0100

    aprrove and deny button

--- Frontend/src/views/VenuePage.tsx
+++ Frontend/src/views/VenuePage.tsx
@@ -2,39 +2,66 @@ import { useState } from "react"
 import AddVenue from "../components/AddVenue"
 import VenueList from "../components/VenueList"
 
-const VenuePage = () => {
-
-    const [currentVenues, setVenues] = useState(
-        [{
-            id: 1,
-            venueName: 'The 02 Arena',
-        },
-        {
-            id: 2,
-            venueName: 'Wembley',
-        },
-        {
-            id: 3,
-            venueName: 'MGM Las Vegas',
-        }]
-    )
-    
-    const [newVenueDivDisplay, setNewVenueDivDisplay] = useState('hide')
-
-    const addVenueClickHandler = () => {
-        newVenueDivDisplay === 'hide'? setNewVenueDivDisplay('show') : setNewVenueDivDisplay('hide')
-    }
-
-    return (
-        <>
-        <h1>ActCiting</h1>
-        <AddVenue handleAddVenueClick={addVenueClickHandler} newVenueDivState={newVenueDivDisplay}/>
-        <VenueList venueArray={currentVenues}/>
-        <footer>
-            <a href="/">Contact Us</a>
-        </footer>
-        </>
-    )
+export type VenuePageProps = {
+  id: number,
+  venueName: string,
+  artistName: string,
+  eventTime: string,
 }
 
-export default VenuePage
+const VenuePage = () => {
+
+  const [currentVenues, setVenues] = useState(
+    [{
+      id: 1,
+      venueName: 'The 02 Arena',
+    },
+    {
+      id: 2,
+      venueName: 'Wembley',
+    },
+    {
+      id: 3,
+      venueName: 'MGM Las Vegas',
+    }]
+  )
+
+  const [newVenueDivDisplay, setNewVenueDivDisplay] = useState('hide')
+
+  const addVenueClickHandler = () => {
+    newVenueDivDisplay === 'hide' ? setNewVenueDivDisplay('show') : setNewVenueDivDisplay('hide')
+  }
+
+  const [approvalStatus, setApprovalStatus] = useState('pending');
+
+  const approveEvent = () => {
+    setApprovalStatus('approved');
+  };
+
+  const denyEvent = () => {
+    setApprovalStatus('denied');
+  };
+
+  return (
+    <>
+      <h1>ActCiting</h1>
+      <AddVenue handleAddVenueClick={addVenueClickHandler} newVenueDivState={newVenueDivDisplay} />
+      <VenueList venueArray={currentVenues} />
+      <footer>
+        <a href="/">Contact Us</a>
+      </footer>
+      <div>
+        {approvalStatus === 'pending' && (
+          <>
+            <button onClick={approveEvent}>Approve</button>
+            <button onClick={denyEvent}>Deny</button>
+          </>
+        )}
+        {approvalStatus === 'approved' && <p>Event approved!</p>}
+        {approvalStatus === 'denied' && <p>Event denied.</p>}
+      </div>
+    </>
+  );
+};
+
+export default VenuePage;
