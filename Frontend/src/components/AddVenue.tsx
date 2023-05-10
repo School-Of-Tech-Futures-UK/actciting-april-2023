0678827773b465a21d20e32739f3a90dea545bad
Uzair Shaikh <Uzair.Shaikh@accenture.com>
2023-05-10 15:45:42 +0100

    addvenue post added for one of the inputs

--- Frontend/src/components/AddVenue.tsx
+++ Frontend/src/components/AddVenue.tsx
@@ -1,19 +1,63 @@
+import { useState } from "react"
+
 export interface AddVenueProps {
     handleAddVenueClick?: () => void
     newVenueDivState: string
+    clickHandler?:() => void
 }
 
 const AddVenue = (props: AddVenueProps) => {
+    const [Name, setValue] = useState('')
+    const [Name, setValue] = useState('')
+
+
+
+    const clickHandler = () => {
+        fetch('http://localhost:3000/venues', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: Name,
+            capacity: event.capacity,
+            // address: event.venueAddress,
+            // geolocation: event.venueGeolocation,
+            // email: event.Email,
+            // start_date: event.venueStartDate,
+            // end_date: event.venueEndDate,
+          })
+        })
+          .then(response => {
+            if (response.status >= 400) {
+              throw new Error(`error status: ${response.status}`)
+            }else{
+                console.log("success")
+            }
+            return response.text()
+          })
+          .catch(error => console.log('there was an error:', error))
+
+          console.log(value)
+      
+
+    }
+
+
+
+
+
+
     return (
         <div>
             <button className='btn btn-primary' type='button' onClick={props.handleAddVenueClick}>Add Venue</button>
 
             <div className={`addVenueForm ${props.newVenueDivState}`} data-testid="venueForm">
                 <label htmlFor="venueName">Venue Name:</label>
-                <input className="form-control" type="text" id="venueName"></input>
+                <input className="form-control" type="text" id="venueName" name = "venueName"  value={value} onInput={(event) => setValue((event.target as HTMLInputElement).value)} ></input>
 
                 <label htmlFor="venueCapacity"> Venue Capacity:</label>
-                <input className="form-control" type="text"></input>
+                <input className="form-control" type="text" id ="capacity"></input>
 
                 <label htmlFor="venueAddress"> Venue Address:</label>
                 <input className="form-control" type="text" id ="venueAddress"></input>
@@ -30,7 +74,7 @@ const AddVenue = (props: AddVenueProps) => {
                 <label htmlFor="venueEndDAte"> Venue End Date</label>
                 <input className="form-control" type="text" id = "venueEndDate"></input>
 
-                <button /*onClick={function to submit data to database }*/>Submit</button>
+                <button onClick={clickHandler }>Submit</button>
             </div>
         </div>
     )
--- Frontend/src/views/VenuePage.tsx
+++ Frontend/src/views/VenuePage.tsx
@@ -12,6 +12,39 @@ const VenuePage = () => {
     const addVenueClickHandler = () => {
         newVenueDivDisplay === 'hide'? setNewVenueDivDisplay('show') : setNewVenueDivDisplay('hide')
     }
+  
+
+    const clickHandler = (event) => {
+        // fetch('http://localhost:3000/venues', {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify({
+        //     name: event.venueName,
+        //     capacity: event.capacity,
+        //     address: event.venueAddress,
+        //     geolocation: event.venueGeolocation,
+        //     email: event.Email,
+        //     start_date: event.venueStartDate,
+        //     end_date: event.venueEndDate,
+        //   })
+        // })
+        //   .then(response => {
+        //     if (response.status >= 400) {
+        //       throw new Error(`error status: ${response.status}`)
+        //     }else{
+        //         console.log("success")
+        //     }
+        //     return response.text()
+        //   })
+        //   .catch(error => console.log('there was an error:', error))
+
+          console.log(event.target.venueName)
+      
+
+    }
+
 
     const fetchVenues = async () => {
         const results = await fetch('http://localhost:3000/venues')
@@ -28,7 +61,7 @@ const VenuePage = () => {
         <>
         <Navbar/>
         <br></br>   
-        <AddVenue handleAddVenueClick={addVenueClickHandler} newVenueDivState={newVenueDivDisplay}/>
+        <AddVenue handleAddVenueClick={addVenueClickHandler} newVenueDivState={newVenueDivDisplay} clickHandler ={clickHandler}/>
         <VenueList venueArray={currentVenues}/>
         <footer>
             <a href="/">Contact Us</a>
