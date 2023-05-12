402a398c0f44dd79ed91699fd033c654058cbd7e
Hajr Delli <hajr.delli@accenture.com>
2023-05-12 16:21:10 +0100

    venueDetails test added/updated

--- Frontend/src/views/VenueDetails.test.tsx
+++ Frontend/src/views/VenueDetails.test.tsx
@@ -8,41 +8,25 @@ import { once } from 'events'
 //test UI when someone clicks delete - sends HTTP request and mock out the call to the database
 describe('DeleteButton', () => {
 
-    jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve()} as Response)) 
- 
- it('testing the mock config', async () => {
- const results = await fetch('http://www.example.com')
- const data = await results.json()
- 
- expect(fetch).toHaveBeenCalledTimes(1)
- expect(fetch).toHaveBeenLastCalledWith('http://www.example.com')
- })
-    // jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve()} as Response)) 
+   beforeEach(() => {
+      jest.spyOn(global, "fetch").mockImplementation(() => Promise.resolve({ json: () => Promise.resolve()} as Response)) 
+   })
 
-    // it('testing the mock config', async () => {
-    //   const results = await fetch('http://www.example.com')
-    //   const data = await results.json()
-
-    //   expect(fetch).toHaveBeenCalledTimes(1)
-    //   expect(fetch).toHaveBeenLastCalledWith('http://www.example.com')
-    // })
-    
-    // it('when delete button clicked, send a HTTP delete request', () => {
-    //     //Arrange
-    //     render(<VenueDetails/>)
-    //     const deleteButton = screen.getByText('Delete')
-    //     fireEvent.click(deleteButton)
+   it('when delete button clicked, send a HTTP delete request', () => {
+        //Arrange
+        render(<VenueDetails/>)
+        const deleteButton = screen.getByText('Delete')
+        fireEvent.click(deleteButton)
         
-    //     //validate that the mock has received the request 
-    //     //Assert
-    //     expect(fetch).toHaveBeenCalledTimes(1);
+        //validate that the mock has received the request 
+        //Assert
+        expect(fetch).toHaveBeenCalledTimes(2);
 
-    //     const firstCall = (fetch as jest.Mock).mock.calls[0]
-    //     const requestProperties = firstCall[1]
-
-    //     expect(requestProperties).toBe(expect.objectContaining({
-    //       method: 'DELETE'
-    //     }))
-    // })
+        expect(fetch).toHaveBeenLastCalledWith(
+         expect.stringContaining('http://localhost:3000/venue'),
+         expect.objectContaining({
+            method: 'DELETE'
+         }))
+    })
   
   })
