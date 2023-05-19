0e40f7f0c459093888b00246e34a95f6f311b710
Richard Crawley <richard.crawley@infinityworks.com>
2023-05-19 17:10:09 +0100

    Disable cloudfront caching for frontend resources

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -365,6 +365,7 @@ new cdk.CfnOutput(this, 'PrettyApiUrl', {
               function: redirectsFunction,
             },
           ],
+      cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED
     },
     additionalBehaviors: {
       '/api/*': { // must be same as default stage name above in ApiGW
